3e7832504267ef9c704d4666aadec5b1761b6a11
Tauqeer Awan <tauqeer.insta@gmail.com>
2019-05-12 11:48:52 +0500

    Add snapshot tests

---
+++ src/__tests__/Stepper.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stepper } from "../lib/Stepper";
+import renderer from "react-test-renderer";
+
+it("renders correctly", () => {
+  const tree = renderer
+    .create(<Stepper steps={["step 1", "step 2"]} currentStep={1} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
