10423d76cc1f28c63a89d8ec2ef36f938af73dc0
Tauqeer Awan <tauqeer.insta@gmail.com>
2019-06-14 12:10:28 +0500

    update snapshots

--- src/__tests__/Stepper.test.js
+++ src/__tests__/Stepper.test.js
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
   const tree = renderer
-    .create(<Stepper steps={["step 1", "step 2"]} currentStep={1} />)
+    .create(<Stepper steps={["step 1", "step 2"]} currentStep={1} onItemClick={() => { }} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
